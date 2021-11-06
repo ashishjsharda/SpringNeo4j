329040230302c4aebe68b40f479586c133e6b1d0
Ashish Sharda <ashishjsharda@gmail.com>
2021-11-06 13:41:00 -0400

    Add other props

--- src/main/java/com/example/nosql/Aircraft.java
+++ src/main/java/com/example/nosql/Aircraft.java
@@ -6,6 +6,8 @@ import org.springframework.data.neo4j.core.schema.GeneratedValue;
 import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
 
+import java.time.Instant;
+
 @Node
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Aircraft {
@@ -21,4 +23,22 @@ public class Aircraft {
     private int vertRate;
     @JsonProperty("selected_altitude")
     private int selectedAltitude;
+
+    private double lat, lon, barometer;
+    @JsonProperty("polar_distance")
+    private double polarDistance;
+    @JsonProperty("polar_bearing")
+    private double polarBearing;
+
+    @JsonProperty("is_adsb")
+    private boolean isADSB;
+    @JsonProperty("is_on_ground")
+    private boolean isOnGround;
+
+    @JsonProperty("last_seen_time")
+    private Instant lastSeenTime;
+    @JsonProperty("pos_update_time")
+    private Instant posUpdateTime;
+    @JsonProperty("bds40_seen_time")
+    private Instant bds40SeenTime;
 }
