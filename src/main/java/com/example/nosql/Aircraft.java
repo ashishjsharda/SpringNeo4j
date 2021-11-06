e3d118893e0bb9de3c695afbd41f244b4df6a497
Ashish Sharda <ashishjsharda@gmail.com>
2021-11-06 13:31:57 -0400

    Add all my files

--- src/main/java/com/example/nosql/Aircraft.java
+++ src/main/java/com/example/nosql/Aircraft.java
@@ -1,9 +1,24 @@
 package com.example.nosql;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.data.neo4j.core.schema.GeneratedValue;
+import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
 
 @Node
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Aircraft {
+    @Id
+    @GeneratedValue
+    private Long neoId;
+
+    private Long id;
+    private String callsign, squawk, reg, flightno, route, type, category;
+
+    private int altitude, heading, speed;
+    @JsonProperty("vert_rate")
+    private int vertRate;
+    @JsonProperty("selected_altitude")
+    private int selectedAltitude;
 }
