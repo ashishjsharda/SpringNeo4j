cd4f6be588696c1d4977774ebcfe422ab8bb5954
Ashish Sharda <ashishjsharda@gmail.com>
2021-11-18 05:47:38 -0500

    Add Lombok dependency ,update annotation

--- src/main/java/com/example/nosql/Aircraft.java
+++ src/main/java/com/example/nosql/Aircraft.java
@@ -2,22 +2,27 @@ package com.example.nosql;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
 import org.springframework.data.neo4j.core.schema.GeneratedValue;
 import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
 
+import javax.persistence.Entity;
 import java.time.Instant;
 import java.util.Objects;
 
 @Node
 @Entity
+@Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Aircraft {
+
+    @Id
+    private Long id;
     @Id
     @GeneratedValue
     private Long neoId;
 
-    private Long id;
     private String callsign, squawk, reg, flightno, route, type, category;
 
     private int altitude, heading, speed;
