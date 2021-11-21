8053b1d1c066c809919c90258acdb4b7741bbe4c
Ashish Sharda <ashishjsharda@gmail.com>
2021-11-21 08:11:30 -0500

    Add getMapping

--- src/main/java/com/example/nosql/PositionController.java
+++ src/main/java/com/example/nosql/PositionController.java
@@ -4,6 +4,7 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @RequiredArgsConstructor
@@ -14,6 +15,7 @@ public class PositionController {
     private WebClient client =
             WebClient.create("http://localhost:7634/aircraft");
 
+    @GetMapping("/aircraft")
     public String getCurrentAircraftPositions(Model model) {
         repository.deleteAll();
         client.get()
