b14b687d076c630d6f43bc69c7d1d1f9c4018a01
Ashish Sharda <ashishjsharda@gmail.com>
2021-11-21 08:08:59 -0500

    Add Aircraft positions method

--- src/main/java/com/example/nosql/PositionController.java
+++ src/main/java/com/example/nosql/PositionController.java
@@ -3,6 +3,7 @@ package com.example.nosql;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @RequiredArgsConstructor
@@ -12,4 +13,9 @@ public class PositionController {
     private final AircraftRepository repository;
     private WebClient client =
             WebClient.create("http://localhost:7634/aircraft");
+
+    public String getCurrentAircraftPositions(Model model) {
+
+    }
+
 }
