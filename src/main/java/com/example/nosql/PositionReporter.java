71db4e3a65cd1f30519df88fb99c6abf631fc2b5
Ashish Sharda <ashishjsharda@gmail.com>
2021-11-29 18:23:16 -0500

    Add all my files

---
+++ src/main/java/com/example/nosql/PositionReporter.java
@@ -0,0 +1,9 @@
+package com.example.nosql;
+
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@AllArgsConstructor
+@Configuration
+public class PositionReporter {
+}
