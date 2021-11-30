47cb0f7e1c846a6a01e3b0a5d7483d3e908ab860
Ashish Sharda <ashishjsharda@gmail.com>
2021-11-30 06:11:11 -0500

    Add retrieve functions

--- src/main/java/com/example/nosql/PositionRetriever.java
+++ src/main/java/com/example/nosql/PositionRetriever.java
@@ -7,4 +7,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class PositionRetriever {
 
+    private final AircraftRepository repo;
+
+    @Bean
+    Consumer<List<Aircraft>> retrieveAircraftPositions() {
+        return acList -> {
+            repo.deleteAll();
+
+            repo.saveAll(acList);
+
+            repo.findAll().forEach(System.out::println);
+        };
 }
