dad97c102b4eb91561a38198ec06b94f30c8f39b
Ashish Sharda <ashishjsharda@gmail.com>
2021-11-30 06:10:11 -0500

    Add Annotations

--- src/main/java/com/example/nosql/PositionRetriever.java
+++ src/main/java/com/example/nosql/PositionRetriever.java
@@ -1,4 +1,10 @@
 package com.example.nosql;
 
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+
+@AllArgsConstructor
+@Configuration
 public class PositionRetriever {
+
 }
