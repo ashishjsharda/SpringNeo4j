e0e069ea0909abe5caf2f9458f8846d0aaa32edc
Ashish Sharda <ashishjsharda@gmail.com>
2021-12-03 06:39:48 -0500

    Add class

---
+++ src/main/java/com/example/nosql/WebSocketHandler.java
@@ -0,0 +1,9 @@
+package com.example.nosql;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class WebSocketHandler {
+}
