0634d36a6ddf0af2a9e36ee781860bbca8c9b8de
Ashish Sharda <ashishjsharda@gmail.com>
2021-12-05 07:17:51 -0500

    Add methods

--- src/main/java/com/example/nosql/WebSocketHandler.java
+++ src/main/java/com/example/nosql/WebSocketHandler.java
@@ -4,6 +4,8 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.socket.WebSocketSession;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.time.Instant;
@@ -21,5 +23,18 @@ public class WebSocketHandler extends TextWebSocketHandler {
         return sessionList;
     }
 
+    public void afterConnectionEstablished(WebSocketSession session)
+            throws Exception {
+        sessionList.add(session);
+        System.out.println("Connection established from " + session.toString() +
+                " @ " + Instant.now().toString());
+    }
+
+    public void afterConnectionClosed(WebSocketSession session,
+                                      CloseStatus status) throws Exception {
+        sessionList.remove(session);
+        System.out.println("Connection closed by " + session.toString() +
+                " @ " + Instant.now().toString());
+    }
 
 }
