0326caa398bf0f09b847f4830be17d0cda04f7f0
Ashish Sharda <ashishjsharda@gmail.com>
2021-12-05 07:14:10 -0500

    Add gradle dependencies

--- src/main/java/com/example/nosql/WebSocketHandler.java
+++ src/main/java/com/example/nosql/WebSocketHandler.java
@@ -1,9 +1,24 @@
 package com.example.nosql;
 
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.socket.WebSocketSession;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Component
-public class WebSocketHandler {
+public class WebSocketHandler extends TextWebSocketHandler {
+    private final List<WebSocketSession> sessionList = new ArrayList();
+    @NonNull
+    private final AircraftRepository repository;
+
+    public List<WebSocketSession> getSessionList() {
+        return sessionList;
+    }
+
+
 }
