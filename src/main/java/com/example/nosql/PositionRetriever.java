afef1774382ca0a7126f780894459d9df4c1b447
Ashish Sharda <ashishjsharda@gmail.com>
2021-12-08 06:41:44 -0500

    Add Web Socket config

--- src/main/java/com/example/nosql/PositionRetriever.java
+++ src/main/java/com/example/nosql/PositionRetriever.java
@@ -3,7 +3,10 @@ package com.example.nosql;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.socket.WebSocketSession;
+import org.springframework.web.socket.TextMessage;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.function.Consumer;
 
@@ -12,6 +15,7 @@ import java.util.function.Consumer;
 public class PositionRetriever  {
 
     private final AircraftRepository repo;
+    private final WebSocketHandler handler;
 
     @Bean
     Consumer<List<Aircraft>> retrieveAircraftPositions() {
@@ -20,5 +24,19 @@ public class PositionRetriever  {
 
 
         };
+    }
+
+    private void sendPositions() {
+        if (repo.count() > 0) {
+            for (WebSocketSession sessionInList : handler.getSessionList()) {
+                try {
+                    sessionInList.sendMessage(
+                            new TextMessage(repo.findAll().toString())
+                    );
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
---
+++ src/main/java/com/example/nosql/WebSocketConfig.java
@@ -0,0 +1,9 @@
+package com.example.nosql;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig {
+}
