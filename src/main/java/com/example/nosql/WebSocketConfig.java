d820deefd369fdb492d84a382ad9c1afe786396e
Ashish Sharda <ashishjsharda@gmail.com>
2021-12-08 06:42:52 -0500

    Add registration

--- src/main/java/com/example/nosql/WebSocketConfig.java
+++ src/main/java/com/example/nosql/WebSocketConfig.java
@@ -2,8 +2,19 @@ package com.example.nosql;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
 @EnableWebSocket
 public class WebSocketConfig {
+    private final WebSocketHandler handler;
+
+    WebSocketConfig(WebSocketHandler handler) {
+        this.handler = handler;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(handler, "/ws");
+    }
 }
